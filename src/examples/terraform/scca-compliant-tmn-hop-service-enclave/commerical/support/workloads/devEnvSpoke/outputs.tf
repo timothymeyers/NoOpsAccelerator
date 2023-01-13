990824fd084b798b669e29644e10bd699a498b6c
John Spinella <jrspinella@users.noreply.github.com>
2023-01-13 05:46:39 -0500

    deleted folder

--- src/examples/terraform/scca-compliant-tmn-hop-service-enclave/commerical/support/workloads/devEnvSpoke/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "wl_subid" {
-  description = "Subscription ID where the Workload Resource Group is provisioned"
-  value       = var.wl_subid
-}
-
-output "wl_vnetname" {
-  description = "The Workload Virtual Network name"
-  value       = module.dev_env_aks_workload_spoke_network.virtual_network_name
-}

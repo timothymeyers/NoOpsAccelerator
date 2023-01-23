563e01d5343c85af7c51990cca1d53fa329dec2c
John Spinella <jrspinella@users.noreply.github.com>
2023-01-23 11:51:15 -0500

    Add EventHub

---
+++ src/terraform/core/overlays/eventHubs/locals.tf
@@ -0,0 +1,25 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+#------------------------------------------------------------
+# Local configuration - Default (required). 
+#------------------------------------------------------------
+
+locals {
+  networks_rules = {
+    subnet_ids = var.allowed_subnet_ids
+    cidrs      = var.allowed_cidrs
+  }
+
+  hubs_auth_rules = flatten([
+    for hub_name, hub in try(var.hubs_parameters, {}) : [
+      for rule in ["listen", "send", "manage"] : {
+        hub_name       = hub_name
+        hub            = hub
+        rule           = rule
+        custom_name    = hub.custom_name
+        authorizations = hub.authorizations
+      }
+    ]
+  ])
+}

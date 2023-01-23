fdde2f6e4550642d6d4b76a9c0b31fcec1e9e622
John Spinella <jrspinella@users.noreply.github.com>
2023-01-23 11:53:35 -0500

    Add Redis

---
+++ src/terraform/core/overlays/redisCaches/locals.tf
@@ -0,0 +1,25 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+#------------------------------------------------------------
+# Local configuration - Default (required). 
+#------------------------------------------------------------
+
+locals {
+  redis_family_map = {
+    Basic    = "C",
+    Standard = "C",
+    Premium  = "P"
+  }
+
+  data_persistence_enabled = var.sku_name == "Premium" ? var.data_persistence_enabled : false
+
+  default_redis_config = {
+    rdb_backup_enabled            = local.data_persistence_enabled
+    rdb_storage_connection_string = one(azurerm_storage_account.redis_storage[*].primary_blob_connection_string)
+    rdb_backup_frequency          = local.data_persistence_enabled ? var.data_persistence_frequency_in_minutes : null
+    rdb_backup_max_snapshot_count = local.data_persistence_enabled ? var.data_persistence_max_snapshot_count : null
+  }
+
+  redis_config = merge(local.default_redis_config, var.redis_configuration)
+}

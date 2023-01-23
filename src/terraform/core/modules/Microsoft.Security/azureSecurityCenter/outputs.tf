00ebba4105ce021fd2c95f73489266740bf40d1e
John Spinella <jrspinella@users.noreply.github.com>
2023-01-23 11:49:04 -0500

    Move/Delete files

--- src/terraform/core/modules/Microsoft.Security/azureSecurityCenter/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "security_center_workspace_id" {
-  description = "The Security Center Workspace resource ID."
-  value       = azurerm_security_center_workspace.main.id
-}
-
-output "security_center_contact_id" {
-  description = "The Security Center Contact ID"
-  value       = azurerm_security_center_contact.main.id
-}
